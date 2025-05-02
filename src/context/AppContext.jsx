59f451281a7e789d9495d5086aea8b1811032ba3
eramit01 <ptamit7826@gmail.com>
2025-05-02 17:21:27 +0530

    hero component added

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,14 +1,23 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
 export const AppContext = createContext();
-export const AppContextProvider = (props) => {
-    const value ={
 
-    }
-return (
+export const AppContextProvider = (props) => {
+  const [searchFilter, setSearchFilter] = useState({
+    title: '',
+    location: '',
+  });
+  const [isSearched, setIsSearched] = useState(false);
+
+  const value = {
+    searchFilter,
+    setSearchFilter,
+    isSearched,setIsSearched
+  };
+
+  return (
     <AppContext.Provider value={value}>
-        {props.children}
+      {props.children}
     </AppContext.Provider>
-);
-  
-}
+  );
+};
